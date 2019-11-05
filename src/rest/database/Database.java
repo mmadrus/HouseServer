b7a96e70e77b9dfc8250e19f6883287fa814dd0b
Isak Zayane <isak.zayane@hotmail.com>
2019-11-05 16:43:40 +0100

    Mongo java driver added .jar
    Some basic methods implemented

---
+++ src/rest/database/Database.java
@@ -0,0 +1,102 @@
+package rest.database;
+
+import java.util.Date;
+
+import com.mongodb.*;
+import org.bson.types.ObjectId;
+import org.json.JSONObject;
+
+/*
+The database class. For now we use a local database, you need to download and start MongoDB as a service, call it "HouseDatabase".
+Use the correct port for your server.
+
+To get the object id, use the method "getObjectId", which returns the object ID. The method uses "our" object id notation (e.g. 1234, as protocol states)
+
+*/
+
+public class Database {
+    private MongoClient mongoClient = null;
+    private DB databaseObj = null;
+    private static Database database = Database.getInstance();
+    private DBCollection dbCollection;
+    private BasicDBObject document;
+    private DBCursor cursor;
+    private DBObject fetchedObject;
+
+
+    public static void main(String[] args) {
+
+        Object objectId = Database.getInstance().getDeviceId("1234");
+        String status = Database.getInstance().getDeviceStatus(objectId);
+        System.out.println(status);
+
+    }
+
+    private Database() {
+        mongoClient = new MongoClient("localhost", 27017);
+        databaseObj = mongoClient.getDB("HouseDatabase");
+
+    }
+
+
+    public static Database getInstance() {
+        if (database == null) {
+
+            database = new Database();
+        }
+        return database;
+
+
+    }
+
+
+    public String getDeviceStatus(Object id) {
+
+        dbCollection = databaseObj.getCollection("Device");
+        document = new BasicDBObject();
+
+        document.put("_id", id);
+        cursor = dbCollection.find(document);
+        fetchedObject = cursor.next();
+
+        return fetchedObject.get("State").toString();
+
+    }
+
+    public Object getDeviceId(String id) {
+        dbCollection = databaseObj.getCollection("Device");
+
+        document = new BasicDBObject();
+        document.put("id", id);
+        cursor = dbCollection.find(document);
+
+        fetchedObject = cursor.next();
+
+
+        return fetchedObject.get("_id");
+    }
+
+
+    //Create user
+
+
+    public void createUser(JSONObject json){
+        /*
+        user Id
+        user Email
+        UserName
+        First name
+        Last name
+        Password
+         */
+
+
+    }
+    //Find user
+    public void findUser(int id){
+
+
+    }
+
+
+}

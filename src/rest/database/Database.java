bc85e8f6373a0337c2325c8775c0cd5b61b2ec3f
Isak Zayane <isak.zayane@hotmail.com>
2019-11-07 11:01:28 +0100

    Mongo java driver added .jar
    Some basic methods implemented
    
    edit: removed .iml from gitignore

--- src/rest/database/Database.java
+++ src/rest/database/Database.java
@@ -10,7 +10,8 @@ import org.json.JSONObject;
 The database class. For now we use a local database, you need to download and start MongoDB as a service, call it "HouseDatabase".
 Use the correct port for your server.
 
-To get the object id, use the method "getObjectId", which returns the object ID. The method uses "our" object id notation (e.g. 1234, as protocol states)
+To get the object id(mongoDB's object ID, looks like: "5dc199596d60d45f6409d791", use the method "getObjectId", which returns the object ID.
+The method uses "our" object id notation (e.g. 1234, as protocol states)
 
 */
 
@@ -29,6 +30,8 @@ public class Database {
         Object objectId = Database.getInstance().getDeviceId("1234");
         String status = Database.getInstance().getDeviceStatus(objectId);
         System.out.println(status);
+
+        Database.getInstance().findUser("1234-abcd-12dc");
 
     }
 
@@ -80,7 +83,7 @@ public class Database {
     //Create user
 
 
-    public void createUser(JSONObject json){
+    public void createUser(JSONObject json) {
         /*
         user Id
         user Email
@@ -92,8 +95,25 @@ public class Database {
 
 
     }
+
     //Find user
-    public void findUser(int id){
+    public Object findUser(String id) {
+        dbCollection = databaseObj.getCollection("User");
+
+        document = new BasicDBObject();
+        document.put("userId", id);
+        cursor = dbCollection.find(document);
+
+        fetchedObject = cursor.next();
+
+
+
+        return fetchedObject;
+
+
+    }
+
+    public void createUser(String jsonString){
 
 
     }
